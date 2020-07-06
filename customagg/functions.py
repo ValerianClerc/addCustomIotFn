33d3fe6c74e73ad973a4224d4488a24f9c69ca66
Valerian Clerc <vclerc@ibm.com>
2020-07-06 13:54:15 -0400

    Revert "Use simpleaggregator"
    This reverts commit 78fcb0188c227e3b4bef7e4f10ae035e40a4b4ed.

--- customagg/functions.py
+++ customagg/functions.py
@@ -1,13 +1,12 @@
-import inspect
+import re
 import logging
 import datetime as dt
 import math
-from sqlalchemy.sql.sqltypes import TIMESTAMP, VARCHAR
 import numpy as np
 import pandas as pd
 
 from iotfunctions.base import BaseSimpleAggregator
-from iotfunctions.ui import (UIExpression,UIMultiItem)
+from iotfunctions.ui import (UIMultiItem,UIExpression)
 
 logger = logging.getLogger(__name__)
 
@@ -16,45 +15,33 @@ logger = logging.getLogger(__name__)
 
 PACKAGE_URL = 'git+https://github.com/ValerianClerc/addCustomIotFn@starter_agg_package'
 
-
-class SimpleAggregatorVC(BaseSimpleAggregator):
+class HelloWorldAggregatorVC(BaseSimpleAggregator):
     '''
-    Create aggregation using expression. The calculation is evaluated for
-    each data_item selected. The data item will be made available as a
-    Pandas Series. Refer to the Pandas series using the local variable named
-    "x". The expression must return a scalar value.
-
-    Example:
-
-    x.max() - x.min()
-
+    The docstring of the function will show as the function description in the UI.
     '''
 
-    def __init__(self, input_items, expression=None, output_items=None):
-        super().__init__()
+    def __init__(self, source=None, expression=None):
+        if expression is None or not isinstance(expression, str):
+            raise RuntimeError("argument expression must be provided and must be a string")
 
-        self.input_items = input_items
+        self.input_items = source
+        self.source = source
         self.expression = expression
-        self.output_items = output_items
+
+    def execute(self, group):
+        return eval(re.sub(r"\$\{GROUP\}", r"group", self.expression))
 
     @classmethod
     def build_ui(cls):
         inputs = []
-        inputs.append(UIMultiItem(name='input_items', datatype=None, description=('Choose the data items'
-                                                                                  ' that you would like to'
+        # Input variable name must be kept 'source'
+        # Output variable name must be kept 'name'
+        inputs.append(UIMultiItem(name='source', datatype=None, description=('Choose the data items'
+                                                                            ' that you would like to'
                                                                                   ' aggregate'),
-                                  output_item='output_items', is_output_datatype_derived=True))
+                                  output_item='name', is_output_datatype_derived=True))
 
-        inputs.append(UIExpression(name='expression', description='Paste in or type an AS expression'))
-
+        inputs.append(UIExpression(name='expression', description='Use ${GROUP} to reference the current grain.'
+                                                    'All Pandas Series methods can be used on the grain.'
+                                                    'For example, ${GROUP}.max() - ${GROUP}.min().'))
         return (inputs, [])
-
-    def _calc(self, df):
-        """
-        If the function should be executed separately for each entity, describe the function logic in the _calc method
-        """
-        return df[self.input_items].apply(self.get_aggregation_method())
-
-    def aggregate(self, x):
-        return eval(self.expression)
-
